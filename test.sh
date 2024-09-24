84c7c8205f62251e44fece15f716f955e0fb8b4b
Malachi <ukomalachi@gmail.com>
2024-09-24 21:17:12 +0100

    add more test criteria in test.sh

--- test.sh
+++ test.sh
@@ -16,3 +16,15 @@
 
 # this check if a file is executable
 [ -x read.sh ]
+
+# this test if a file exists in any form.
+[ -e <any_file> ]
+
+# this tests if the file is readable
+[ -r <any_file> ]
+
+# this check if file1 is newer than file2
+[  file1 -nt file2 ]
+
+# this checks if file1 is older than file2
+[ file -ot file2 ]
